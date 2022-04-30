e97e5297b683940ee5f2db73fb4391796df06720
VIPIN YADAV <78799997+vipinyd@users.noreply.github.com>
2022-04-30 16:40:50 +0530

    ca comply

--- app/Models/Traderlog.php
+++ app/Models/Traderlog.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Traderlog extends Model
 {
   protected $table = "traderlogs";
+
+  protected static function boot()
+  {
+      parent::boot();
+   
+      // Order by name ASC
+      static::addGlobalScope('order', function (Builder $builder) {
+          $builder->orderBy('id', 'desc');
+      });
+  }
+
 
   public function user()
   {
